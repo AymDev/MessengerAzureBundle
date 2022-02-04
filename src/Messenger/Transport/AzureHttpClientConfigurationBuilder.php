89fa9d821cb94e05fd7103a99cab5bfa3bd2047c
AymDev <aymericmayeux@gmail.com>
2022-02-04 18:50:03 +0100

    Create HTTP Client configuration builder

---
+++ src/Messenger/Transport/AzureHttpClientConfigurationBuilder.php
@@ -0,0 +1,148 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AymDev\MessengerAzureBundle\Messenger\Transport;
+
+/**
+ * Configuration builder for the Azure transports HTTP clients
+ * @internal
+ */
+final class AzureHttpClientConfigurationBuilder
+{
+    /**
+     * Build configuration for a sender HttpClient transport
+     * @param array{
+     *     transport_name: string,
+     *     entity_path: string,
+     *     subscription?: string|null,
+     *     token_expiry: int
+     *     receive_mode: string
+     * } $options
+     * @return array{endpoint: string, options: mixed[]}
+     */
+    public function buildSenderConfiguration(string $dsn, array $options): array
+    {
+        return $this->buildConfiguration(false, $dsn, $options);
+    }
+
+    /**
+     * Build configuration for a receiver HttpClient transport
+     * @param array{
+     *     transport_name: string,
+     *     entity_path: string,
+     *     subscription?: string|null,
+     *     token_expiry: int
+     *     receive_mode: string
+     * } $options
+     * @return array{endpoint: string, options: mixed[]}
+     */
+    public function buildReceiverConfiguration(string $dsn, array $options): array
+    {
+        return $this->buildConfiguration(true, $dsn, $options);
+    }
+
+    /**
+     * @param array{
+     *     transport_name: string,
+     *     entity_path: string,
+     *     subscription?: string|null,
+     *     token_expiry: int
+     *     receive_mode: string
+     * } $options
+     * @return array{endpoint: string, options: mixed[]}
+     */
+    private function buildConfiguration(bool $isReceiver, string $dsn, array $options): array
+    {
+        [$sharedAccessKeyName, $sharedAccessKey, $namespace] = $this->parseDsn($dsn, $options['transport_name']);
+        $endpoint = $this->getBaseEndpoint($isReceiver, $namespace, $options['entity_path'], $options['subscription']);
+
+        $sasToken = $this->generateSharedAccessSignatureToken(
+            $endpoint,
+            $sharedAccessKeyName,
+            $sharedAccessKey,
+            $options['token_expiry']
+        );
+
+        $clientOptions = [
+            'endpoint' => $endpoint,
+            'options' => [
+                'headers' => [
+                    'Authorization' => $sasToken,
+                ],
+            ]
+        ];
+
+        if (!$isReceiver) {
+            $clientOptions['options']['headers']['Content-Type'] = 'application/atom+xml;type=entry;charset=utf-8';
+        }
+
+        return $clientOptions;
+    }
+
+    /**
+     * Parse the DSN to extract the namespace and shared access key
+     * @return string[]
+     */
+    private function parseDsn(string $dsn, string $transportName): array
+    {
+        if (1 !== preg_match('~^azure://(.+):(.+)@(.+)$~', $dsn, $matches)) {
+            $message = sprintf('Invalid Azure Service Bus DSN for the "%s" transport. ', $transportName);
+            $message .= 'It must be in the following format: azure://SharedAccessKeyName:SharedAccessKey@namespace';
+            throw new \InvalidArgumentException($message, 1643988474);
+        }
+
+        array_shift($matches);
+        return $matches;
+    }
+
+    /**
+     * Build the base endpoint URL used by HTTP client and SAS token
+     */
+    private function getBaseEndpoint(
+        bool $isReceiver,
+        string $namespace,
+        string $entityPath,
+        ?string $subscription
+    ): string {
+        // Endpoint for a topic receiver transport
+        if ($isReceiver && null !== $subscription) {
+            return sprintf(
+                'https://%s.servicebus.windows.net/%s/subscriptions/%s/',
+                $namespace,
+                $entityPath,
+                $subscription
+            );
+        }
+
+        return sprintf('https://%s.servicebus.windows.net/%s/', $namespace, $entityPath);
+    }
+
+    /**
+     * Generate the SAS token used to authenticate on Azure Service Bus REST API.
+     */
+    private function generateSharedAccessSignatureToken(
+        string $endpoint,
+        string $accessKeyName,
+        string $accessKey,
+        int $tokenExpiry
+    ): string {
+        // Token expiry instant
+        $expiry = time() + $tokenExpiry;
+
+        // URL-encoded URI of the resource being accessed
+        $resource = strtolower(rawurlencode(strtolower($endpoint)));
+
+        // URL-encoded HMAC SHA256 signature
+        $toSign = $resource . "\n" . $expiry;
+        $signature = rawurlencode(base64_encode(hash_hmac('sha256', $toSign, $accessKey, true)));
+
+        return sprintf(
+            'SharedAccessSignature sig=%s&se=%d&skn=%s&sr=%s',
+            $signature,
+            $expiry,
+            $accessKeyName,
+            $resource
+        );
+    }
+}
---
+++ tests/Messenger/Transport/AzureHttpClientConfigurationBuilderTest.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\AymDev\MessengerAzureBundle\Messenger\Transport;
+
+use AymDev\MessengerAzureBundle\Messenger\Transport\AzureHttpClientConfigurationBuilder;
+use PHPUnit\Framework\TestCase;
+
+final class AzureHttpClientConfigurationBuilderTest extends TestCase
+{
+    /**
+     * Check default configurations
+     * @dataProvider provideClientConfiguration
+     * @param mixed[] $configuration
+     */
+    public function testDefaultConfiguration(bool $isSender, array $configuration): void
+    {
+        self::assertIsArray($configuration);
+        self::assertArrayHasKey('endpoint', $configuration);
+        self::assertArrayHasKey('options', $configuration);
+
+        // It MUST end with a trailing slash
+        self::assertSame('https://namespace.servicebus.windows.net/entity/', $configuration['endpoint']);
+
+        if ($isSender) {
+            self::assertArrayHasKey('Content-Type', $configuration['options']['headers']);
+        }
+
+        self::assertArrayHasKey('headers', $configuration['options']);
+        self::assertArrayHasKey('Authorization', $configuration['options']['headers']);
+        self::assertStringStartsWith(
+            'SharedAccessSignature ',
+            $configuration['options']['headers']['Authorization']
+        );
+    }
+
+    /**
+     * @return mixed[][]
+     */
+    public function provideClientConfiguration(): array
+    {
+        $dsn = 'azure://KeyName:Key@namespace';
+        $options = [
+            'transport_name' => 'test-transport',
+            'entity_path' => 'entity',
+            'subscription' => null,
+            'token_expiry' => 3600,
+            'receive_mode' => 'peek-lock',
+        ];
+
+        $factory = new AzureHttpClientConfigurationBuilder();
+
+        return [
+            [true, $factory->buildSenderConfiguration($dsn, $options)],
+            [false, $factory->buildReceiverConfiguration($dsn, $options)],
+        ];
+    }
+
+    /**
+     * The endpoint for a receiver client for a topic must include the subscription
+     */
+    public function testTopicReceiverConfiguration(): void
+    {
+        $factory = new AzureHttpClientConfigurationBuilder();
+        $configuration = $factory->buildReceiverConfiguration('azure://KeyName:Key@namespace', [
+            'transport_name' => 'test-transport',
+            'entity_path' => 'entity',
+            'subscription' => 'subscription',
+            'token_expiry' => 3600,
+            'receive_mode' => 'peek-lock',
+        ]);
+
+        // It MUST end with a trailing slash
+        self::assertSame(
+            'https://namespace.servicebus.windows.net/entity/subscriptions/subscription/',
+            $configuration['endpoint']
+        );
+    }
+
+    /**
+     * The DSN must respect a specific format
+     */
+    public function testThrowsOnInvalidDsn(): void
+    {
+        self::expectException(\InvalidArgumentException::class);
+        self::expectExceptionCode(1643988474);
+
+        $factory = new AzureHttpClientConfigurationBuilder();
+        $factory->buildSenderConfiguration('azure://invalid-dsn', [
+            'transport_name' => 'test-transport',
+            'entity_path' => 'entity',
+            'token_expiry' => 3600,
+            'receive_mode' => 'peek-lock'
+        ]);
+    }
+}
