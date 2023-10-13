41c371dd4f6fb6c7ee152b024683bea01316197d
Pepa Martinec <josef.martinec@keboola.com>
2023-10-13 12:48:12 +0200

    Allow passing all supported configuration using DSN and options

--- src/Messenger/Transport/AzureTransportFactory.php
+++ src/Messenger/Transport/AzureTransportFactory.php
@@ -14,17 +14,6 @@ use Symfony\Component\Messenger\Transport\TransportInterface;
  */
 final class AzureTransportFactory implements TransportFactoryInterface
 {
-    private const RECEIVE_MODES = [
-        AzureTransport::RECEIVE_MODE_PEEK_LOCK,
-        AzureTransport::RECEIVE_MODE_RECEIVE_AND_DELETE,
-    ];
-    private const DEFAULT_OPTIONS = [
-        'entity_path' => null,
-        'subscription' => null,
-        'token_expiry' => 3600,
-        'receive_mode' => AzureTransport::RECEIVE_MODE_PEEK_LOCK,
-    ];
-
     /** @var DsnParser */
     private $dsnParser;
 
@@ -57,11 +46,16 @@ final class AzureTransportFactory implements TransportFactoryInterface
      */
     public function createTransport(string $dsn, array $options, SerializerInterface $serializer): TransportInterface
     {
-        $options = $this->validateOptions($options);
-        $dsnParts = $this->dsnParser->parseDsn($dsn, $options['transport_name']);
+        $transportName = $options['transport_name'];
+        if (!is_string($transportName)) {
+            throw new \InvalidArgumentException('The "transport_name" option must be set.');
+        }
+        unset($options['transport_name']);
 
-        $senderConfiguration = $this->httpClientConfigurationBuilder->buildSenderConfiguration($dsnParts, $options);
-        $receiverConfiguration = $this->httpClientConfigurationBuilder->buildReceiverConfiguration($dsnParts, $options);
+        $options = $this->dsnParser->parseDsn($dsn, $options, $transportName);
+
+        $senderConfiguration = $this->httpClientConfigurationBuilder->buildSenderConfiguration($options);
+        $receiverConfiguration = $this->httpClientConfigurationBuilder->buildReceiverConfiguration($options);
 
         return new AzureTransport(
             $serializer,
@@ -71,54 +65,5 @@ final class AzureTransportFactory implements TransportFactoryInterface
             $options['entity_path'],
             $options['subscription']
         );
-    }
-
-    /**
-     * Validate options and set default values
-     * @param mixed[] $options
-     * @return array{
-     *     transport_name: string,
-     *     entity_path: string,
-     *     subscription: string|null,
-     *     token_expiry: int,
-     *     receive_mode: string
-     * }
-     */
-    private function validateOptions(array $options): array
-    {
-        // Set default values
-        /**
-         * @var array{
-         *     transport_name: string,
-         *     entity_path: string|null,
-         *     subscription: string|null,
-         *     token_expiry: int,
-         *     receive_mode: string
-         * } $options
-         */
-        $options = array_merge(self::DEFAULT_OPTIONS, $options);
-
-        // Missing topic or queue name
-        if (null === $options['entity_path']) {
-            throw new \InvalidArgumentException(
-                sprintf('Missing entity_path (queue or topic) for the "%s" transport.', $options['transport_name']),
-                1643989596
-            );
-        }
-
-        // Invalid receive mode
-        if (false === in_array($options['receive_mode'], self::RECEIVE_MODES, true)) {
-            throw new \InvalidArgumentException(
-                sprintf(
-                    'Invalid "%s" receive_mode for the "%s" transport. It must be one of: %s.',
-                    $options['receive_mode'],
-                    $options['transport_name'],
-                    implode(', ', self::RECEIVE_MODES)
-                ),
-                1643994036
-            );
-        }
-
-        return $options;
     }
 }
